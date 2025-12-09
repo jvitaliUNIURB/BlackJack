597a29ef64f6c9b95795e3dea7c893729e469054
jvitaliUNIURB <j.vitali@campus.uniurb.it>
2025-12-09 12:44:47 +0100

    Delete src directory

--- src/Const/Const.java
+++
@@ -1,8 +0,0 @@
-package Const;
-
-public interface Const {
-    final static int CLUBS = 0;
-    final static int HEARTS = 1;
-    final static int DIAMONDS = 2;
-    final static int SPADES = 3;
-}
