4b357d5d9958bc7b8784e7c86e31d8654469b834
jonas <j.vitali@campus.uniurb.it>
2025-12-09 11:57:05 +0100

    test te test

---
+++ src/Const/Const.java
@@ -0,0 +1,8 @@
+package Const;
+
+public interface Const {
+    final static int CLUBS = 0;
+    final static int HEARTS = 1;
+    final static int DIAMONDS = 2;
+    final static int SPADES = 3;
+}
